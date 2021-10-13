aab3d1574242fa1ed3c11b183aa28a85082d5436
iva-anastas <anastasiaivanina61@gmail.com>
2021-10-13 11:06:18 +0300

    commit my

---
+++ app.js
@@ -0,0 +1,11 @@
+let express = require('express');
+let app = express();
+let indexRoute = require('./routers/indexRoute');
+let bookRoute = require('./routers/bookRoute');
+let filmRoute = require('./routers/filmRoute');
+app.set('view-engine', 'ejs');
+app.use(express.static('public'));
+app.use('/', indexRoute);
+app.use('/book', bookRoute);
+app.use('/film', filmRoute);
+app.listen(3000);
